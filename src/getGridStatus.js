4759cb9f76e23117c1acd56866d006f87ec5769d
Martin Nebel <martin.nebel@johnlewis.com>
2019-05-31 13:51:13 +0100

    0.2 Whose turn is it next

--- src/getGridStatus.js
+++ src/getGridStatus.js
@@ -2,10 +2,13 @@ function getGridStatus(inputGridArray) {
 
     const noTokensOnThisRow = inputGridRow => inputGridRow === ".......";
     const emptyGrid = inputGridArray => inputGridArray.every(noTokensOnThisRow);
+    const redWentLast = inputGridArray => inputGridArray.some(inputGridRow => inputGridRow.indexOf('R') != -1);
 
     if (emptyGrid(inputGridArray)) return "Red plays next";
-
-    return "Who knows?";
+    
+    return redWentLast(inputGridArray)
+        ? "Yellow plays next"
+        : "Red plays next";
 }
 
 module.exports = getGridStatus;
--- test/getGridStatus.test.js
+++ test/getGridStatus.test.js
@@ -5,3 +5,8 @@ test('Empty grid', () => {
    expect(getGridStatus(['.......', '.......', '.......', '.......', '.......', '.......']))
             .toBe('Red plays next');
 });
+
+test('Grid with one red token', () => {
+   expect(getGridStatus(['.......', '.......', '.......', '.......', '.......', '...R...']))
+      .toBe('Yellow plays next');
+});
