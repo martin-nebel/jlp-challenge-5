5cbdb08f6bf2aa420a43b9c5d9ef080935b2df85
Martin Nebel <martin.nebel@johnlewis.com>
2019-05-31 14:08:16 +0100

    0.3 Red wins horizontally

--- src/getGridStatus.js
+++ src/getGridStatus.js
@@ -2,10 +2,17 @@ function getGridStatus(inputGridArray) {
 
     const noTokensOnThisRow = inputGridRow => inputGridRow === ".......";
     const emptyGrid = inputGridArray => inputGridArray.every(noTokensOnThisRow);
-    const redWentLast = inputGridArray => inputGridArray.some(inputGridRow => inputGridRow.indexOf('R') != -1);
+
+    const redWentLastOnRow = inputGridRow => inputGridRow.indexOf('R') !== -1;
+    const redWentLast = inputGridArray => inputGridArray.some(redWentLastOnRow);
+
+    const redWinsRow = inputGridRow => inputGridRow.toUpperCase().indexOf('RRRR') !== -1;
+    const redWinsHorizontally = inputGridArray => inputGridArray.some(redWinsRow);
 
     if (emptyGrid(inputGridArray)) return "Red plays next";
-    
+
+    if (redWinsHorizontally(inputGridArray)) return "Red wins";
+
     return redWentLast(inputGridArray)
         ? "Yellow plays next"
         : "Red plays next";
--- test/getGridStatus.test.js
+++ test/getGridStatus.test.js
@@ -2,11 +2,42 @@ const getGridStatus = require('../src/getGridStatus');
 
 //--------------------------------------------------------------------------------
 test('Empty grid', () => {
-   expect(getGridStatus(['.......', '.......', '.......', '.......', '.......', '.......']))
+   expect(getGridStatus(['.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '.......']))
             .toBe('Red plays next');
 });
 
 test('Grid with one red token', () => {
-   expect(getGridStatus(['.......', '.......', '.......', '.......', '.......', '...R...']))
+   expect(getGridStatus(['.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '...R...']))
       .toBe('Yellow plays next');
 });
+
+test('Grid with one red and one yellow token', () => {
+   expect(getGridStatus(['.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '...rY..']))
+      .toBe('Red plays next');
+});
+
+test('Red wins horizontally', () => {
+   expect(getGridStatus(['.......', 
+                         '.......', 
+                         '.......', 
+                         '.......', 
+                         '..yyy..', 
+                         '..rrrR.']))
+      .toBe('Red wins');
+});
+
